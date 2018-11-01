820756b2b5df774cf15fc996044794f5d5588c08
manish <mgupta9151@gmail.com>
2018-11-01 09:11:25 +0530

    summery data

--- professional_review.py
+++ professional_review.py
@@ -21,7 +21,8 @@ logger=logging.getLogger()
 
 BASE_URL = "http://professional-review.com/"
 COUNTRY_LIST=[]
-HEADER_LIST = ["Country", "Title", "Update Logo", "Update Header", "Update Link", "Update Date", "Update Content"]
+HEADER_LIST = ["Country", "State", "Store Name", "Address", "Website URL",
+"Tel Phone", "Email", "About", "Description"]
 data = OrderedDict()
 FILE_NAME = os.path.join(os.getcwd(),"report/professional-review_{}.xlsx".format(int(time.time())))
 
@@ -33,11 +34,11 @@ def get_country():
 		soup = BeautifulSoup(response, 'html.parser');
 		links = soup.find_all("a", class_="p-2 text-muted")
 		for link in links:
-			COUNTRY_LIST.append({"name":link.text,"link":BASE_URL+link['href']})
+			COUNTRY_LIST.append({"country":link['href'].split("/")[0].title(), "state": link.text,"link":BASE_URL+link['href']})
+		print(COUNTRY_LIST)
 		logger.info("#----country find success----")
 	else:
 		logger.error("#----country find request failed.----")
-
 
 def get_details():
 	get_country()
@@ -50,27 +51,35 @@ def get_details():
 			
 			updates = soup.find_all("div", class_="card flex-md-row mb-4 box-shadow h-md-200")
 			for update in updates:
-				logo = update.find('img')['src']
-				header = update.find('strong').find('a').text
+				print(country)
 				header_link = BASE_URL+update.find('strong').find("a")['href']
-				update_date = update.find('div', class_='text-muted').text.split(" ")[-1]
-				message = update.find('p', class_='card-text').text
-				# get update data 
+				print("update request failed {}".format(header_link))
 				update_req = requests.get(header_link)
 				if update_req.status_code == 200:
-					update_response = update_req.content;
-					update_soup = BeautifulSoup(update_response, 'html.parser');
-					latest_updates = soup.find_all("div", class_="card flex-md-row mb-4 box-shadow h-md-200")
-					for update_data in latest_updates:
-						title = soup.select("h1.display-4")[0].text
-						title_desc = soup.find("p", class_="lead my-3").text
-						header_links = soup.find_all("a")
-					
+					try:
+						update_response = update_req.text;
+						update_soup = BeautifulSoup(update_response, 'html.parser');
+						header_sec = update_soup.find("div", class_="jumbotron")
+						store_name = header_sec.find("h1", class_="display-4").text.decode('utf-8','ignore').strip()
+						address = header_sec.find("p", class_="lead my-3").text.decode('utf-8','ignore').strip()
+						web_url = header_sec.find_all("a")[0]['href'].decode('utf-8','ignore').strip()
+						tel_phone = header_sec.find_all("a")[1]['href'].decode('utf-8','ignore').strip()
+						email = update_soup.find_all("li", class_="text-info")[0].text
+						about_desc = update_soup.find_all("p", class_="mb-0")
+						about = about_desc[0].text.replace('\n', ' ')
+						description =  ""
+						if about_desc[1]:
+							description = about_desc[1].text.decode('utf-8','ignore').strip().replace('\n', ' ')
 
-				UPDATE_DATA.append([country['name'], title, logo, header, header_link, update_date, message ])
-			data.update({"Sheet {}".format(country['name']): UPDATE_DATA})
+						UPDATE_DATA.append([country['country'],country['state']
+						, store_name, address, web_url, tel_phone, email, about, description ])
+					except:
+						pass
+				else:
+					print("update request failed {}".format(header_link))
+			data.update({"Sheet {}".format(country['country']): UPDATE_DATA})
 	else:
-		logger.error("#----country {0} find details failed link {1}.----".format(country['name'], country['link']))
+		logger.error("#----country {0} find details failed link {1}.----".format(country['country'], country['link']))
 
 
 def format_file():
@@ -83,7 +92,7 @@ def format_file():
 	for ws in wb.worksheets:
 		width = 15
 		ws.row_dimensions[0].height = 30
-		for column in ["A", "B", "C", "D", "E", "F", "G"]:
+		for column in ["A", "B", "C", "D", "E", "F", "G", "H", "I"]:
 			ws.column_dimensions[column].width = width
 			width+=10
 
